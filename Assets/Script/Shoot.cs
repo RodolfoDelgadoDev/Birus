583305a8384decc258da80aa44dd1a248796bbfb
RodolfoDelgadoDev <rodrigo.delgado3132@gmail.com>
2025-01-25 12:17:49 -0300

    Adding weapon and limited clip
    20 bullets per clip
    missing animation (fix soon)

--- Assets/Script/Shoot.cs
+++ Assets/Script/Shoot.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     [SerializeField] private Transform origin;
+
+    private int remainingBullets;
+    
+    [SerializeField] GameObject weapon;
+
+    
+
+    private Animator animator;
+
+
     [SerializeField] private GameObject bubble;
 
     private GameObject bubbleIn;
@@ -13,7 +23,8 @@ public class Shoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        remainingBullets = 20;
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,8 +32,22 @@ public class Shoot : MonoBehaviour
     {
         if( Input.GetMouseButtonDown(0))
         {
+            if(remainingBullets > 0)
+            {
             bubbleIn = Instantiate(bubble, origin.position, origin.rotation);
             bubbleIn.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0,30f,100f), ForceMode.Force);
+            remainingBullets--;
+            Debug.Log("CAPITO ME QUEDAN " + remainingBullets + "BALAAAAASSS");
+            }
+            else
+            {
+                Debug.Log("TENGO QUE CARGAR LOCO AAAAAAAAAAAAAAAA");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            remainingBullets = 20;
+            Debug.Log("CARGADO MI LOCO!");
         }
     }
 }
