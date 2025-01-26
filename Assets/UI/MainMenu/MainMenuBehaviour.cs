8898da52b5c163ade53c50c329bee00528f2e177
Revelion-santi <Revetriasanti@gmail.com>
2025-01-26 16:24:39 -0300

    Main Menu and Win Menu with sounds

--- Assets/UI/MainMenu/MainMenuBehaviour.cs
+++ Assets/UI/MainMenu/MainMenuBehaviour.cs
@@ -6,15 +6,44 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuBehaviour : MonoBehaviour
 {
-    //Starts the Game
-    public void PlayGame ()
+    public AudioSource menuSound;
+
+    // Starts the game with a smoother transition
+    public void PlayGame()
     {
-        SceneManager.LoadScene("GameMap");
+        // Play the menu sound
+        menuSound.Play();
+
+        // Start a coroutine to handle the scene change with a slight delay
+        StartCoroutine(LoadSceneWithDelay("GameMap", 1.0f ));
     }
 
+    IEnumerator LoadSceneWithDelay(string sceneName, float delay)
+    {
+        // Wait for the specified delay
+        yield return new WaitForSeconds(delay);
+
+        // Load the target scene
+        SceneManager.LoadScene(sceneName);
+    }
+   
+    
     //Quits the Game
     public void QuitGame()
     {
+        // Play the menu sound
+        menuSound.Play();
+
+        // Start a coroutine to handle the scene change with a slight delay
+        StartCoroutine(QuitSceneWithDelay(1.0f));
+    }
+
+    IEnumerator QuitSceneWithDelay(float delay)
+    {
+        // Wait for the specified delay
+        yield return new WaitForSeconds(delay);
+        //Quit app
+        Debug.Log("app end");
         Application.Quit();
     }
 }
